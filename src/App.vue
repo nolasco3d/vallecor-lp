4b1e7c63e91fcae7139a883381ba7f40b1fee8a2
André Nolasco <nolasco3d@gmail.com>
2022-08-22 13:58:00 -0300

    Finish Mobile Adjusts

--- src/App.vue
+++ src/App.vue
@@ -4,43 +4,60 @@
 
 <template>
   <main class="">
-    <section
-      class="bg-[#4F008C] bg-[url(src/assets/hero.png)] bg-no-repeat bg-right-top">
-      <div class="container mx-auto text-white py-24 max-w-6xl">
-        <img class="badge" src="src/assets/badge-white.svg"
+    <section class="
+        bg-[#4F008C] 
+          
+          md:bg-[url(src/assets/hero.png)] 
+          md:bg-right-top md:bg-[length:720px_auto]
+          bg-no-repeat">
+      <div class="container mx-auto text-white  md:py-24 max-w-6xl">
+        <img class="badge hidden md:block" src="src/assets/badge-white.svg"
           alt="Selo leinertex">
-        <div class="columns-1">
-          <img class="mb-16" src="src/assets/logo-white-.svg"
+
+        <div class="columns-1 relative text-center md:text-left">
+          <!-- LOGO MOBILE -->
+          <img class="md:hidden h-7 absolute top-8 left-6"
+            src="src/assets/logo-color.svg" alt="Logo vallecor mobile">
+
+          <!-- LOGO DESKTOP -->
+          <img class="mb-16 hidden md:block" src="src/assets/logo-white.svg"
             alt="Logo vallecor">
 
-          <h2 class="mb-16 text-5xl">Economia e qualidade <br />
+          <!-- HERO MOBILE -->
+          <img class="md:hidden" src="src/assets/hero_mobile.png"
+            alt="Hero Mobile">
+
+          <h2 class="mb-8 md:mb-16 pt-32 text-3xl md:text-5xl md:pt-0  ">
+            Economia e qualidade <br />
             <strong class="font-black">do jeito que você<br />
               sempre sonhou</strong>
           </h2>
 
           <button class="btn large blue mb-20">Conheça nossa linha</button>
 
-          <h3 class="mb-20 text-4xl">A escolha certa para quem <br />quer uma
-            <strong class="font-black">vida mais colorida</strong>.
+          <h3 class="mb-10 md:mb-20 text-2xl md:text-4xl">
+            A escolha certa para quem <br />
+            quer uma <strong class="font-black">vida mais colorida</strong>.
           </h3>
         </div>
 
-        <div class="flex items-start justify-evenly">
-          <div class="icon-wrapper">
-            <img class="icon" src="src/assets/icon-coin.svg" alt="Icon">
+        <div class="grid grid-cols-2 gap-y-8 pb-8 md:pb-4 md:grid-cols-4">
+          <div class="text-center font-black flex flex-col self-baseline">
+            <img class="icon mx-auto" src="src/assets/icon-coin.svg" alt="Icon">
             Economia na hora <br />de pintar
           </div>
-          <div class="icon-wrapper">
-            <img class="icon" src="src/assets/icon-bucket-pencil.svg"
+          <div class="text-center font-black flex flex-col self-baseline">
+            <img class="icon mx-auto" src="src/assets/icon-bucket-pencil.svg"
               alt="Icon">
             Fácil de <br />aplicar
           </div>
-          <div class="icon-wrapper">
-            <img class="icon" src="src/assets/icon-roll.svg" alt="Icon">
+          <div class="text-center font-black flex flex-col self-baseline">
+            <img class="icon mx-auto" src="src/assets/icon-roll.svg" alt="Icon">
             Boa Cobertura
           </div>
-          <div class="icon-wrapper">
-            <img class="icon" src="src/assets/icon-bucket.svg" alt="Icon">
+          <div class="text-center font-black flex flex-col self-baseline">
+            <img class="icon mx-auto" src="src/assets/icon-bucket.svg"
+              alt="Icon">
             Cores variadas <br />para ambientes <br />mais bonitos
           </div>
         </div>
@@ -48,29 +65,35 @@
 
     </section>
 
-    <section class="text-center py-20">
+    <section class="text-center pt-8 md:py-20">
 
-      <h4 class="font-black text-4xl text-[#434343] mb-11">
+      <h4 class="font-black text-2xl md:text-4xl text-[#434343] mb-11">
         Conheça nossas linhas de produtos
       </h4>
 
-      <div class="container mx-auto max-w-6xl flex flex-col gap-12">
-        <div class="bg-[#8B2FAE] card-product flex-row-reverse justify-between">
-          <img class="card-product__image " src="src/assets/Group 126.png"
-            alt="Product" />
+      <div class="flex overflow-x-scroll md:overflow-x-hidden 
+        px-3 gap-4 md:gap-12 pt-[80px] pb-7 md:pt-0 md:pb-0
+        md:container md:mx-auto md:max-w-6xl md:flex-col ">
+
+        <div
+          class="bg-[#8B2FAE] rounded-lg md:flex min-w-[320px] md:w-auto text-white px-5 md:px-10 py-5 flex-row-reverse justify-between">
+          <img class="card-product__image mt-[-100px] md:mt-0"
+            src="src/assets/Group 126.png" alt="Product" />
 
           <div
-            class="card-product__text-wrapper text-left flex flex-1 gap-5 justify-around">
+            class="xxx md:text-left flex flex-col md:flex-row flex-1 gap-5 justify-around">
             <div class="flex flex-col justify-around">
-              <h5 class="text-5xl font-black mb-1">Fosco Ideal</h5>
+              <h5 class="text-4xl md:text-5xl font-black mb-1">Fosco Ideal</h5>
 
               <p class="text-sm mb-5">Disponibilidade:</p>
-              <div class="flex items-baseline gap-8">
+
+              <div
+                class="flex items-baseline justify-center md:justify-start gap-8">
                 <img src="src/assets/bucket-size-icon-36l.svg" alt="">
                 <img src="src/assets/bucket-size-icon-15l.svg" alt="">
               </div>
 
-              <div class="flex">
+              <div class="hidden md:flex">
                 <button class="btn yellow">Ver Cores</button>
                 <button class="btn-download">
                   <span class="material-symbols-outlined">download</span>
@@ -82,30 +105,46 @@
               </div>
             </div>
 
-            <div class="flex flex-col justify-around font-black max-w-[180px]">
+            <div
+              class="flex flex-col justify-around  font-black max-w-[180px] gap-y-5 md:gap-y-1 text-left self-center md:self-auto">
               <p>Economia e qualidade para sua casa</p>
               <p>Excelente rendimento e acabamento</p>
               <p>Parede mais bonita, disfarça leves imperfeições*</p>
             </div>
+
+            <div class="flex md:hidden">
+              <button class="btn yellow">Ver Cores</button>
+              <button class="btn-download">
+                <span class="material-symbols-outlined">download</span>
+                <p class="m-0 pl-2 flex flex-col">
+                  <strong class="font-black">DOWNLOAD</strong>
+                  FICHA TÉCNICA
+                </p>
+              </button>
+            </div>
           </div>
         </div>
 
-        <div class="bg-[#37C0C9] card-product flex justify-between">
+        <div
+          class="bg-[#37C0C9] rounded-lg md:flex min-w-[320px] md:w-auto text-white px-10 py-5 justify-between">
           <img class="card-product__image " src="src/assets/Group 130.png"
             alt="Product" />
 
           <div
-            class="card-product__text-wrapper text-left flex flex-1 gap-5 justify-around">
+            class="xxx text-center md:text-left flex flex-col md:flex-row flex-1 gap-5 justify-around">
             <div class="flex flex-col justify-around">
-              <h5 class="text-5xl font-black mb-1">Massa Corrida</h5>
+              <h5 class="text-4xl md:text-5xl font-black mb-1">Massa Corrida
+              </h5>
 
               <p class="text-sm mb-5">Disponibilidade:</p>
-              <div class="flex items-baseline gap-8">
+
+              <div
+                class="flex items-baseline justify-center md:justify-start gap-8">
                 <img src="src/assets/bucket-size-icon-36l.svg" alt="">
                 <img src="src/assets/bucket-size-icon-15l.svg" alt="">
               </div>
 
-              <div class="flex">
+              <div class="hidden md:flex">
                 <button class="btn yellow">Ver Cores</button>
                 <button class="btn-download">
                   <span class="material-symbols-outlined">download</span>
@@ -124,22 +163,26 @@
           </div>
         </div>
 
-        <div class="bg-[#FF9E32] card-product flex-row-reverse justify-between">
+        <div
+          class="bg-[#FF9E32] rounded-lg md:flex min-w-[320px] md:w-auto text-white px-10 py-5 flex-row-reverse justify-between">
           <img class="card-product__image " src="src/assets/Group 154.png"
             alt="Product" />
 
           <div
-            class="card-product__text-wrapper text-left flex flex-1 gap-5 justify-around">
+            class="xxx text-center md:text-left flex flex-col md:flex-row flex-1 gap-5 justify-around">
             <div class="flex flex-col justify-around">
-              <h5 class="text-5xl font-black mb-1">Esmalte Sintético</h5>
+              <h5 class="text-4xl md:text-5xl font-black mb-1">Esmalte Sintético
+              </h5>
 
               <p class="text-sm mb-5">Disponibilidade:</p>
-              <div class="flex items-baseline gap-8">
+
+              <div
+                class="flex items-baseline justify-center md:justify-start gap-8">
                 <img src="src/assets/bucket-size-icon-36l.svg" alt="">
                 <img src="src/assets/bucket-size-icon-15l.svg" alt="">
               </div>
 
-              <div class="flex">
+              <div class="hidden md:flex">
                 <button class="btn yellow">Ver Cores</button>
                 <button class="btn-download">
                   <span class="material-symbols-outlined">download</span>
@@ -162,14 +205,14 @@
       </div>
     </section>
 
-    <section class="flex text-white">
-      <div class="bg-[#13467E] flex-1">
+    <section class="flex text-white flex-wrap">
+      <div class="bg-[#13467E] md:flex-1 w-full">
         <div class="flex justify-end">
           <img src="src/assets/sofa.png" alt="Imagem sofá" />
         </div>
 
-        <div class="max-w-sm mx-auto my-10">
-          <h6 class="text-4xl mb-5">Vale a pena <br />
+        <div class="max-w-sm mx-auto my-10 px-6 md:p-0">
+          <h6 class="text-3xl md:text-4xl mb-5">Vale a pena <br />
             <strong class="font-black">mudar de cor!</strong>
           </h6>
           <p>
@@ -179,13 +222,13 @@
         </div>
       </div>
 
-      <div class="bg-[#37C0C9] flex-1">
+      <div class="bg-[#37C0C9] md:flex-1 w-full">
         <div class="flex justify-end">
           <img src="src/assets/pintor.png" alt="Imagem sofá">
         </div>
 
-        <div class="max-w-sm mx-auto my-10">
-          <h6 class="text-4xl mb-5">
+        <div class="max-w-sm mx-auto my-10 px-6 md:p-0">
+          <h6 class="text-3xl md:text-4xl mb-5">
             <strong class="font-black">Você é pintor?</strong><br />
             Então vai se apaixonar!
           </h6>
@@ -200,13 +243,14 @@
     </section>
 
     <section class="flex flex-center text-center">
-      <div class="container mx-auto py-11 max-w-4xl">
-        <div class="font-black text-4xl text-[#434343] my-11 ">Dúvidas
-          Frequentes</div>
+      <div class="container mx-auto py-11 max-w-4xl px-5 md:px-0">
+        <div class="font-black text-2xl md:text-4xl text-[#434343] mb-11">
+          Dúvidas Frequentes
+        </div>
 
         <div class="accordion border-b text-left">
           <div
-            class="text-[#13467E] font-black text-xl flex justify-between py-5">
+            class="text-[#13467E] font-black text-xl flex items-start md:items-center justify-between py-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elitaecenas id ex
             porttitor?
             <span
@@ -245,11 +289,12 @@
               quis aliquet.</p>
           </div>
         </div>
+
       </div>
     </section>
 
     <section class="bg-[#4F008C] py-16 text-white relative min-h-0 h-auto">
-      <img class="absolute wid top-[-48px] right-0 "
+      <img class="hidden md:block absolute top-[-48px] right-0 "
         src="src/assets/right-decorator.svg" alt="Decorator">
       <div class="container mx-auto text-center max-w-3xl">
 
@@ -259,7 +304,8 @@
           <span class="font-black">Cadastre-se!</span>
         </h4>
 
-        <form action="" class="grid grid-cols-2 gap-x-8 gap-y-4 mb-10">
+        <form action=""
+          class="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-4 mb-10 px-5">
           <div class="flex flex-col items-start">
             <label class="ml-2 font-black" for="name">Seu nome</label>
             <input
@@ -290,17 +336,23 @@
       </div>
     </section>
   </main>
-  <footer class="relative flex justify-around py-12">
-    <img class="absolute top-0 translate-y-[-50%] left-0"
+
+  <footer class="relative flex justify-between px-6 md:justify-around py-12">
+    <img class=" hidden md:block absolute top-0 translate-y-[-50%] left-0"
       src="src/assets/frame-form-before.svg" alt="Decorator">
-    <img class="absolute top-0 left-[50%] translate-x-[-50%]"
+
+    <img
+      class=" w-[100px] md:w-auto absolute top-0 left-[50%] translate-x-[-50%]"
       src="src/assets/badge-purple.svg" alt="Decorator">
 
-    <img class="mr-[170px]" src="src/assets/logo-color-.svg"
-      alt="Logo Vallecor">
+    <img class="h-[26px] md:h-auto md:mr-[170px]"
+      src="src/assets/logo-color.svg" alt="Logo Vallecor">
 
     <div class="flex flex-col">
-      <span class="text-gray-500">Central de Atendimento</span>
+      <span
+        class="text-gray-500 max-w-[100px] md:max-w-none leading-none">Central
+        de
+        Atendimento</span>
       <a class="font-black" href="tel:08007044244">0800 704 4244</a>
     </div>
   </footer>
@@ -348,7 +400,7 @@ section {
 }
 
 .btn-download {
-  @apply flex items-center px-4 py-1 leading-none text-[#FFE600];
+  @apply flex items-center text-sm px-4 py-1 leading-none text-[#FFE600];
 }
 
 .icon-wrapper {
